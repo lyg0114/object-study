c9b28e0a0d7f93b29e12cb5c79d851fed7eaeade
lyg0114 <whdnseowkd@naver.com>
2022-02-27 08:38:59 +0900

    add Invitation class

---
+++ src/main/java/_chapter01/Invitation.java
@@ -0,0 +1,12 @@
+package _chapter01;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author : iyeong-gyo
+ * @package : _chapter01
+ * @since : 2022/02/27
+ */
+public class Invitation {
+  private LocalDateTime when;
+}

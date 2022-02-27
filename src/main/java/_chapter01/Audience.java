c279daaf353e244f8b3ac4b7191b94258d2a0611
lyg0114 <whdnseowkd@naver.com>
2022-02-27 10:28:20 +0900

    encapsulation 'buy' method at Audience

--- src/main/java/_chapter01/Audience.java
+++ src/main/java/_chapter01/Audience.java
@@ -6,6 +6,7 @@ package _chapter01;
  * @since : 2022/02/27
  */
 public class Audience {
+
   private Bag bag;
 
   public Audience(Bag bag) {
@@ -15,4 +16,16 @@ public class Audience {
   public Bag getBag() {
     return bag;
   }
+
+  public Long buy(Ticket ticket) {
+    if (bag.hasInvitation()) {
+      bag.setTicket(ticket);
+      return 0L;
+    } else {
+      bag.setTicket(ticket);
+      bag.minusAmount(ticket.getFee());
+      return ticket.getFee();
+    }
+  }
+
 }

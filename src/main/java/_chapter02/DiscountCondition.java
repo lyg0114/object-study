49a992b27748fe0b2a9a970949833413efbcc548
lyg0114 <whdnseowkd@naver.com>
2022-03-01 16:42:00 +0900

    make DiscountPolicy abstract class

---
+++ src/main/java/_chapter02/DiscountCondition.java
@@ -0,0 +1,13 @@
+package _chapter02;
+
+/**
+ * @author : iyeong-gyo
+ * @package : _chapter02
+ * @since : 2022/03/01
+ */
+public class DiscountCondition {
+
+  public boolean isSatisfiedBy(Screening screening) {
+    return false;
+  }
+}
--- src/main/java/_chapter02/DiscountPolicy.java
+++ src/main/java/_chapter02/DiscountPolicy.java
@@ -1,13 +1,31 @@
 package _chapter02;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import sun.swing.BakedArrayList;
+
 /**
  * @author : iyeong-gyo
  * @package : _chapter02
  * @since : 2022/03/01
  */
-public class DiscountPolicy {
+public abstract class DiscountPolicy {
+
+  private List<DiscountCondition> conditions = new ArrayList<>();
+
+  public DiscountPolicy(DiscountCondition... conditions) {
+    this.conditions = Arrays.asList(conditions);
+  }
 
   public Money calculateDiscountAmount(Screening screening) {
-    return null;
+    for (DiscountCondition each : conditions) {
+      if (each.isSatisfiedBy(screening)) {
+        return getDiscountAmount(screening);
+      }
+    }
+    return Money.ZERO;
   }
+
+  abstract protected Money getDiscountAmount(Screening screening);
 }

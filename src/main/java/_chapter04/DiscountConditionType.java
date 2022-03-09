3ebbf0b4fd3c1ed0b7dd46082f82528b0494b056
lyg0114 <whdnseowkd@naver.com>
2022-03-10 01:27:10 +0900

    add DiscountConditionType enum

---
+++ src/main/java/_chapter04/DiscountConditionType.java
@@ -0,0 +1,11 @@
+package _chapter04;
+
+/**
+ * @author : iyeong-gyo
+ * @package : _chapter04
+ * @since : 2022/03/10
+ */
+public enum DiscountConditionType {
+  SEQUENCE, // 순번 조건
+  PERIOD // 기간 조건
+}

7a96909d8ed8a8c6cfa0898d03c30494805287e6
lyg0114 <whdnseowkd@naver.com>
2022-03-10 01:08:20 +0900

    add MovieType enum

--- src/main/java/_chapter04/MovieType.java
+++ src/main/java/_chapter04/MovieType.java
@@ -5,6 +5,8 @@ package _chapter04;
  * @package : _chapter04
  * @since : 2022/03/10
  */
-public class MovieType {
-
+public enum MovieType {
+  AMOUNT_DISCOUNT, // 금액 할인 정책
+  PERCENT_DISCOUNT, // 비율 할인 정책
+  NONE_DISCOUNT // 미적용
 }

2f6c1b92d71c54c670fa3961156926857b583b2f
lyg0114 <whdnseowkd@naver.com>
2022-03-10 01:20:28 +0900

    add modifier in Movie class

--- src/main/java/_chapter04/Movie.java
+++ src/main/java/_chapter04/Movie.java
@@ -2,6 +2,7 @@ package _chapter04;
 
 import _chapter02.Money;
 import _chapter02.discountcondition.DiscountCondition;
+import java.util.Collections;
 import java.util.List;
 import javafx.util.Duration;
 
@@ -20,4 +21,33 @@ public class Movie {
   private MovieType movieType;
   private Money discountAmount;
   private Money discountPercent;
+
+
+  public void setMovieType(MovieType movieType) {
+    this.movieType = movieType;
+  }
+
+  public Money getFee() {
+    return fee;
+  }
+
+  public void setFee(Money fee) {
+    this.fee = fee;
+  }
+
+  public List<DiscountCondition> getDiscountConditions() {
+    return Collections.unmodifiableList(discountConditions);
+  }
+
+  public Money getDiscountAmount() {
+    return discountAmount;
+  }
+
+  public void setDiscountAmount(Money discountAmount) {
+    this.discountAmount = discountAmount;
+  }
+
+  public void setDiscountPercent(Money discountPercent) {
+    this.discountPercent = discountPercent;
+  }
 }

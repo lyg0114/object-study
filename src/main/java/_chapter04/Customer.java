91c5f60928343e51c58b9b6f7dd9885b9cc61733
lyg0114 <whdnseowkd@naver.com>
2022-03-11 00:04:17 +0900

    add Customer

--- src/main/java/_chapter04/Customer.java
+++ src/main/java/_chapter04/Customer.java
@@ -7,4 +7,11 @@ package _chapter04;
  */
 public class Customer {
 
+  private String name;
+  private String id;
+
+  public Customer(String name, String id) {
+    this.name = name;
+    this.id = id;
+  }
 }

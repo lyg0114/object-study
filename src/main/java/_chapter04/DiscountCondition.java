6493cea3b7ba6ec6d196911b8921fcdaa0f278cb
lyg0114 <whdnseowkd@naver.com>
2022-03-10 23:58:30 +0900

    add getter setter at DiscountCondition

--- src/main/java/_chapter04/DiscountCondition.java
+++ src/main/java/_chapter04/DiscountCondition.java
@@ -9,6 +9,7 @@ import java.time.LocalTime;
  * @since : 2022/03/10
  */
 public class DiscountCondition {
+
   private DiscountConditionType type;
 
   private int sequence;
@@ -16,4 +17,44 @@ public class DiscountCondition {
   private DayOfWeek dayOfWeek;
   private LocalTime startTime;
   private LocalTime endTime;
+
+  public DiscountConditionType getType() {
+    return type;
+  }
+
+  public void setType(DiscountConditionType type) {
+    this.type = type;
+  }
+
+  public int getSequence() {
+    return sequence;
+  }
+
+  public void setSequence(int sequence) {
+    this.sequence = sequence;
+  }
+
+  public DayOfWeek getDayOfWeek() {
+    return dayOfWeek;
+  }
+
+  public void setDayOfWeek(DayOfWeek dayOfWeek) {
+    this.dayOfWeek = dayOfWeek;
+  }
+
+  public LocalTime getStartTime() {
+    return startTime;
+  }
+
+  public void setStartTime(LocalTime startTime) {
+    this.startTime = startTime;
+  }
+
+  public LocalTime getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(LocalTime endTime) {
+    this.endTime = endTime;
+  }
 }

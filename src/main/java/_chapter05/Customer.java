ec5af8eea5a93790e718f271b94eea132614a9f6
lyg0114 <whdnseowkd@naver.com>
2022-03-17 22:26:39 +0900

    add Screening, Customer, Reservation

---
+++ src/main/java/_chapter05/Customer.java
@@ -0,0 +1,10 @@
+package _chapter05;
+
+/**
+ * @author : iyeong-gyo
+ * @package : _chapter05
+ * @since : 2022/03/17
+ */
+public class Customer {
+
+}
---
+++ src/main/java/_chapter05/Screening.java
@@ -0,0 +1,14 @@
+package _chapter05;
+
+/**
+ * @author : iyeong-gyo
+ * @package : _chapter05
+ * @since : 2022/03/17
+ */
+public class Screening {
+
+  public Reservation reserve(Customer customer, int audienceCount) {
+    return null;
+  }
+
+}
